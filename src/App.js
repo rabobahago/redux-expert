cf601d4553e0855a1b504ed569c3ba25849f071e
rabobahago <rabobahago@gmail.com>
2021-02-10 21:19:03 +0100

    map state and dispatch to props added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import update_person from './store/actions/personActions'
 import update_game from './store/actions/gameActions'
 import update_day from './store/actions/dayInWeek'
 import { connect } from 'react-redux'
-const App = () => {
+const App = (props) => {
+  console.log(props)
   return (
     <div className="App">
       <h2>Redux expect</h2>
-      Person Name: {store.getState().person.name}
-      <button onClick={() => store.dispatch(update_person())}>
-        update person
-      </button>
+      Person Name: {props.person.name}
+      <button onClick={props.updatePerson}>update person</button>
       <br />
-      Game Name: {store.getState().skill.game}
-      <button onClick={() => store.dispatch(update_game())}>update game</button>
+      Game Name: {props.skill.game}
+      <button onClick={props.updateSkill}>update game</button>
       <br />
-      Day in a Week: {store.getState().week.day}
-      <button onClick={() => store.dispatch(update_day())}>update day</button>
+      Day in a Week: {props.week.day}
+      <button onClick={props.updateWeek}>update day</button>
     </div>
   )
 }
 const mapStateToProps = (state) => {
-  return state
+  return {
+    person: state.person,
+    skill: state.skill,
+    week: state.week,
+  }
 }
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = (state) => {
+  return {
+    updatePerson: () => {
+      store.dispatch(update_person())
+    },
+    updateSkill: () => {
+      store.dispatch(update_game())
+    },
+    updateWeek: () => {
+      store.dispatch(update_day())
+    },
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
